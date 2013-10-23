c9bb60342d8ded7740dd6b6c7cade7123b7e4f92
frankpolimi <fvetro94@gmail.com>
2013-10-23 08:31:56 +0200

    Update palindromo.c

--- funzioni1/palindromo.c
+++ funzioni1/palindromo.c
@@ -2,9 +2,6 @@ int palindromo(int numero){
     
     int numero_palindromo=0;	
     int copia_numero = numero; //copio il parametro della funzione per eseguire i calcoli
-    
-    if(numero < 10) //il numero palindromo deve avere obbligatoriamente almeno 2 cifre
-      return 0;
 
   	for(; copia_numero > 0; copia_numero = copia_numero / 10)
       numero_palindromo = numero_palindromo * 10 + copia_numero % 10;
