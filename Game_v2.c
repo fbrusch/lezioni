2642bc1a55ed087544cd8e65338b7a9debdfb2e4
emaema94 <manu.riccia@hotmail.it>
2013-10-28 15:32:39 +0100

    Update Game_v2.c

--- Game_v2.c
+++ Game_v2.c
@@ -1,76 +1,30 @@
+#include <stdio.h>
+
 int vincente(int n)
 {
-	int i;
-  	if (n<3) return 1;  
-  	if (n<6) return 0;
-  	for(i=0;i<=n;i++)
+        int i;
+          if (n<3) return 1;  
+          if (n<6) return 0;
+          for(i=0;i<n;i++)
       if (vincente(i)==0 && vincente(n-i)==0)
-      	return 1;
-	return 0;
+              return 1;
+        return 0;
 }
 
-int codifica(int numero1, int numero2)
-{
-  return numero1*1000+numero2;
-}
-
-int decodifica1(int numero)
-{
-  return numero/1000;
-}
-
-int decodifica2(int numero)
-{
-  return numero%1000;
-}
-
-int decodifica(int numero, int param)
-{
-  if(param==1)
-    return numero/1000;
-  return numero%1000;
-}
-
-int p1a(int numero1, int numero2)
-{
-    return numero1;
-}
-
-int p1b(int numero)
-{
-  	return codifica(numero-3,3);
-}
-
-
-int p2a(int numero1, int numero2)
-{
-    return numero1;
-}
-
-int p2b(int numero)
-{
-  	return codifica(numero-4,4);
-}
-
-int mossa(int numero1, int numero2, int giocatore)
-{
-    int risultato;
+int main(){
+    int num;
     
-    if(giocatore==1)
-    {
-	   risultato = p1a(numero1, numero2);
-       risultato = p1b(risultato);
-    }
-  	else
-    {
-	   risultato = p2a(numero1, numero2);
-       risultato = p2b(risultato);
-    }
+    for(num=0;num<40;num++){
+                            if(vincente(num)==1){
+                                                 printf("il numero %d è vincente! \n",num);
+                                                 }else{
+                                                       printf("il numero %d è perdente! \n",num);
+                                                       }
+                            
+                            }
     
-    giocatore=3-giocatore;
     
-    if(decodifica1(risultato)<3 || decodifica2(risultato)<3)
-        return giocatore;
-    
-    return mossa(decodifica1(risultato), decodifica2(risultato), giocatore);
+    getch();
+    return 0;   
 }
+
